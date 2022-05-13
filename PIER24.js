2485cd65cd0c450a5e400f166bc5ebaa4e51e2c4
roaahaz <105523731+roaahaz@users.noreply.github.com>
2022-05-14 01:17:58 +0300

    Add files via upload

---
+++ PIER24.js
@@ -0,0 +1,18 @@
+function likeFunction(x) {
+  x.style.fontWeight = "bold";
+  x.innerHTML = "✓ Liked";
+}
+
+
+  function home() {
+  location.replace("file:///C:/Users/rwaah/OneDrive/%D8%B3%D8%B7%D8%AD%20%D8%A7%D9%84%D9%85%D9%83%D8%AA%D8%A8/PIER24/PIER24PH.html#home")
+}
+function photo() {
+  location.replace("file:///C:/Users/rwaah/OneDrive/%D8%B3%D8%B7%D8%AD%20%D8%A7%D9%84%D9%85%D9%83%D8%AA%D8%A8/PIER24/Photos.html")
+}
+function about() {
+  location.replace("file:///C:/Users/rwaah/OneDrive/%D8%B3%D8%B7%D8%AD%20%D8%A7%D9%84%D9%85%D9%83%D8%AA%D8%A8/PIER24/about.html")
+}
+function visit() {
+  location.replace("file:///C:/Users/rwaah/OneDrive/%D8%B3%D8%B7%D8%AD%20%D8%A7%D9%84%D9%85%D9%83%D8%AA%D8%A8/PIER24/visitus.html")
+}
